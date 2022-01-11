400ee02434d1b30d6531830d2614d14e09dcdac9
Armando Guardado <armando.guardado.ag@gmail.com>
2022-01-10 21:18:03 -0800

    Set up outline for handman

---
+++ Project/Hangman.py
@@ -0,0 +1,17 @@
+
+ # Task 1:  Title is displayed
+ # Task 2:  Prompt user for name
+#  Task 3:  Prompt user if they want to see Stats (read from file) or begin Playing, or quit
+    #  IF player opts to see stats, will access file and print details
+    # IF player opts  to quit, current score will be saved if higher than previous score
+    #  IF player opts to play
+        #generate workd and underscore values with image
+        #prompts user to letter or to solve
+#           If user chooses letter:
+ #              If user guess right letter. then they are displayed with effect to handmand image
+#               if user guess wrong letter, then hangman image changes
+#             If user chooses to solve
+#                 If guess is right then congratulating message
+#                 if guess is wrong then hangman image changes
+
+
