f883a87018a641098243857f5cf039dcdcf52f3c
Armando Guardado <armando.guardado.ag@gmail.com>
2022-01-12 21:42:22 -0800

    file to hold dict of different menus

---
+++ Project/menus.py
@@ -0,0 +1,16 @@
+main_menu = {
+    1: 'Returning Player',
+    2: 'New Player',
+    3: 'Quit Game',
+}
+
+sub_menu = {
+    1: 'Find Previous Record',
+    2: 'Return to Previous Menu',
+}
+
+opening_menu = {
+    1: 'Play Hangman',
+    2: 'See Scoreboard (Top 5 Players)',
+    3: 'Quit Game',
+}
