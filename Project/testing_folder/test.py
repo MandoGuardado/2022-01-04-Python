80fed9fd6422b9a24364534a1d3fb849b4c7c135
Armando Guardado <armando.guardado.ag@gmail.com>
2022-01-13 21:23:52 -0800

    testing

--- Project/testing_folder/test.py
+++ Project/testing_folder/test.py
@@ -5,11 +5,17 @@ def start():
     csv_file = "user_data1.csv"
     users_df = pd.read_csv(csv_file)
     print(users_df)
-    user_df = users_df.loc[users_df['user_name'] != 'tazan']
+    print("\n")
+    user_df = users_df.loc[users_df['user_name'] != 'tarzan']
     print(user_df)
+    print("\n")
     user_df.to_csv('user_data1.csv', index=False)
+    print(pd.read_csv(csv_file))
 
-    print()
+    with open('user_data1.csv', 'a') as fd:
+        fd.write(f'\ntesttest,test,test123,25')
+    print("\n")
+    print(pd.read_csv(csv_file))
 
 
 if __name__ == '__main__':
